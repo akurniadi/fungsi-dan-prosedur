dbe38cea2e35230e6ff61f79bcf4d56cabbb90cf
akurniadi <addi.kurniadi@gmail.com>
2015-03-30 23:27:07 +0700

    tugas

---
+++ src/id/co/blits/fungsidanprosedur/TugasFungsiProsedur.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.blits.fungsidanprosedur;
+
+/**
+ *
+ * @author akurniadi
+ */
+public class TugasFungsiProsedur {
+
+    public static int penjumlahan(int angka1,int angka2) {//membuat funsi baru 
+        
+        int hitung = angka1 + angka2;
+        return hitung;
+        
+    }
+   public class MethodBilanganGanjil{//untuk mengembalikan nilai 
+       public void bilanganGanjil(int nilaiPengurangan){//membuat prosedur baru
+       }
+       }  
+    public static void main(String[] args) {
+        System.out.println("Hasil Penjumlahannya adalah : "+penjumlahan(10,7));//untuk menampilkan dan menginputan nilai untuk proses penjumlahan
+        System.out.println("*********************************");
+        int nilaiPengurangan = 10-7;//inputan nilai untuk pengurangan
+    
+        System.out.println("Hasil Pengurangannya adalah : "+nilaiPengurangan);//menampilkan hasil pengurangan
+
+    }
+    
+}
